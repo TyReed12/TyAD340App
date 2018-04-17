ca876bc298b8838a1c45dbba25d9bc66ab96df80
[tyler] <[tyreed12@gmail.com]>
2018-04-17 12:42:01 -0700

    HW 2 with logcat messages

--- app/src/main/java/com/example/tyree/tyad340app/MainActivity.java
+++ app/src/main/java/com/example/tyree/tyad340app/MainActivity.java
@@ -5,13 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Log Errors, Debug, and Warning messages
+        Log.e(TAG, "OnCreate Method Error");
+        Log.d(TAG, "OnCreate Method Debug");
+        Log.w(TAG, "OnCreate Method Warning");
+
+
     }
 
     /** Called when the user taps the Send button */
@@ -21,5 +32,10 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+        //Log Errors, Debug, and Warning messages
+        Log.e(TAG, "sendMessage Method Error");
+        Log.d(TAG, "sendMessage Method Debug");
+        Log.w(TAG, "sendMessage Method Warning");
     }
 }
